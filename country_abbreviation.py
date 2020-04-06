596418e82fb329bd79f4a0cfe24a0e183f6800d4
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-04-06 14:47:43 +0800

    abbreivation

---
+++ country_abbreviation.py
@@ -0,0 +1,23 @@
+
+# coding=utf8
+
+'''
+version: April 06, 2020 02:11 PM
+Last revision: April 06, 2020 02:30 PM
+
+Author : Chao-Hsuan Ke
+'''
+
+'''
+country dict
+'''
+
+
+Taiwan_Chinese_dict = {'台灣': 'tw', '臺灣': 'tw', '呆丸': 'tw', '台湾': 'tw'}
+China_Chinese_dict = {'中國': 'cn', '大陸': 'cn', '老共': 'cn', '中国': 'cn'}
+Japan_Chinese_dict = {'日本': 'jp'}
+America_Chinese_dict = {'美國': 'us', '美国': 'us', '米國': 'us', '米国': 'us'}
+
+
+print(Taiwan_Chinese_dict.get('臺灣'))
+#print(China_Chinese_dict.get('中國'))
