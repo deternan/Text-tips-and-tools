9ca4bc8424c5f366a2904a3b33ebf7802f21b262
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-04-06 15:02:30 +0800

    add check function

--- country_abbreviation.py
+++ country_abbreviation.py
@@ -3,7 +3,7 @@
 
 '''
 version: April 06, 2020 02:11 PM
-Last revision: April 06, 2020 02:30 PM
+Last revision: April 06, 2020 03:00 PM
 
 Author : Chao-Hsuan Ke
 '''
@@ -19,5 +19,34 @@ Japan_Chinese_dict = {'日本': 'jp'}
 America_Chinese_dict = {'美國': 'us', '美国': 'us', '米國': 'us', '米国': 'us'}
 
 
-print(Taiwan_Chinese_dict.get('臺灣'))
+#print(Taiwan_Chinese_dict.get('臺灣'))
 #print(China_Chinese_dict.get('中國'))
+
+
+def get_abbreviation(country):
+    getAbb = None
+    tagCheck = False
+    if Taiwan_Chinese_dict.get(country) != None:
+        getAbb = Taiwan_Chinese_dict.get(country)
+        tagCheck = True
+
+    if China_Chinese_dict.get(country) != None:
+        getAbb = China_Chinese_dict(country)
+        tagCheck = True
+
+    if Japan_Chinese_dict.get(country) != None:
+        getAbb = Japan_Chinese_dict.get(country)
+        tagCheck = True
+
+    if America_Chinese_dict.get(country) != None:
+        getAbb = America_Chinese_dict.get(country)
+        tagCheck = True
+
+
+    if tagCheck == True:
+        print(getAbb)
+    else :
+        print('None')
+
+
+get_abbreviation('阿根廷')
